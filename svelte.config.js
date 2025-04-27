edaf1adc1700a4027d7be239cca5ac7d24c350f4
Annie Lin <yiln996@aucklanduni.ac.nz>
2025-04-27 23:45:09 +1200

    push

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,20 @@
+import path from 'path';
+import sveltePreprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html'  // For SPA (Single Page Application) behavior
+			pages: 'build',
+            assets: 'build',
 		}),
 		// Optionally, add the base path for GitHub Pages if your repo is not named 'username.github.io'
 		paths: {
 			base: '/myFirstWebPage' // Replace with the actual repository name
 		}
-	}
+	},
+	preprocess: sveltePreprocess(),
 };
 
 export default config;
